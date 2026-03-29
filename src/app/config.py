c61e50bc6b602aba389913455eab4610bfe5753e
martin <martin.cadez0@gmail.com>
2026-03-29 23:19:45 +0200

    fix: update port container name

--- src/app/config.py
+++ src/app/config.py
@@ -11,7 +11,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_HOST_PORT")
+DB_PORT = os.getenv("DB_CONTAINER_PORT")
 
 DB_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DB_URI)
